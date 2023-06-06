c02b8a3564b039df5275eceba65263ba2c97c3f5
Faysal Ahmed <faysalahmed.dev@gmail.com>
2023-06-07 02:46:39 +0600

    feat: :building_construction: add route guard base on user
    Signed-off-by: Faysal Ahmed <faysalahmed.dev@gmail.com>

---
+++ src/routes/+layout.ts
@@ -0,0 +1,12 @@
+import { redirect } from '@sveltejs/kit';
+import type { LayoutLoad } from './$types';
+
+export const load: LayoutLoad = async ({ data, url }) => {
+	if (data.user && url.pathname === '/login') {
+		throw redirect(307, '/');
+	}
+	if (!data.user && url.pathname !== 'login') {
+		throw redirect(307, '/login');
+	}
+	return { user: data.user };
+};
